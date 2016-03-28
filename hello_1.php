7cf8f48de3d097e71b8f6b6b85087cb084d610c3
Example <example@example.com>
2016-03-28 14:06:31 +0000

    Hello world example.

---
+++ hello_1.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// This is our first line of PHP code.
+print "Hello, world!"; // end of line comment
+
+/*
+ * Another multi-line comment.
+ * Another line.
+ */
+
+?>
