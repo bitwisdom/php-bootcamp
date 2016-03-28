dced7b2abb0addc60fb561357cc9b96bd40d8764
Example <example@example.com>
2016-03-28 14:24:18 +0000

    Hello world example with HTML.

---
+++ hello_2.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * PHP with HTML example
+ */
+
+?>
+<html>
+    <head>
+        <title>
+            <?php 
+                print "Hello, world!"; 
+            ?>
+        </title>
+    </head>
+    <body>
+        <h1><?php print "Hello, world!"; ?></h1>
+    </body>
+</html>
