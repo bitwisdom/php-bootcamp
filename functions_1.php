18ed08c0497b88bf34e9bbab5f1de112eaeead82
Wayne Eaker <wayne@zengenuity.com>
2016-04-08 12:20:06 -0400

    Simple print function.

--- functions_1.php
+++ functions_1.php
@@ -1,16 +1,19 @@
 <?php
-$name = "Bob";
+
+function print_greeting() {
+    print "Welcome Joe";
+}
 
 ?>
 
 <html>
     <head>
         <title>
-            <?php print "Welcome $name"; ?> 
+            <?php print_greeting(); ?> 
         </title>
     </head>
     <body>
-        <?php print "Welcome $name"; ?>
+        <?php print_greeting(); ?>
     </body>
 </html>
 
