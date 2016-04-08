62313e65687220e3a73df9c743029a8dd371680c
Wayne Eaker <wayne@zengenuity.com>
2016-04-08 12:11:43 -0400

    Simple non-function-having code.

---
+++ functions_1.php
@@ -0,0 +1,16 @@
+<?php
+$name = "Bob";
+
+?>
+
+<html>
+    <head>
+        <title>
+            <?php print "Welcome $name"; ?> 
+        </title>
+    </head>
+    <body>
+        <?php print "Welcome $name"; ?>
+    </body>
+</html>
+
