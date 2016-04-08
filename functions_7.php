0c888de5cd2648f028a4e84ce0990e0be71c09ef
Wayne Eaker <wayne@zengenuity.com>
2016-04-08 14:57:44 -0400

    Add reference parameter example.

---
+++ functions_7.php
@@ -0,0 +1,18 @@
+<?php
+
+function addOne(&$y) {
+  $y = $y + 1;
+  return $y;
+}
+
+$x = 5;
+
+?>
+
+<html>
+  <body>
+    X plus one is <?php print addOne($x); ?>.
+    X plus one is <?php print addOne($x); ?>.
+  </body>
+</html>
+
