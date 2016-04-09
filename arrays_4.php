eba1ddd51fa8b5fdc02379a0720935eb4e255786
Wayne Eaker <wayne@zengenuity.com>
2016-04-09 14:44:19 -0400

    Sorting indexed arrays

---
+++ arrays_4.php
@@ -0,0 +1,14 @@
+<?php
+
+// There are built-in PHP functions for sorting arrays different ways.
+
+// Sorting Indexed Arrays
+$names = ["jenny", "peter", "bob", "joe"];
+var_dump($names);
+
+sort($names);
+var_dump($names);
+
+rsort($names);
+var_dump($names);
+
