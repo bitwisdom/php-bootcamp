968c8e0e9887c9fdafbd71cc29a962adb1f4eeed
Wayne Eaker <wayne@zengenuity.com>
2016-04-10 13:29:04 -0400

    While loop example.

---
+++ loops_1.php
@@ -0,0 +1,13 @@
+<?php
+
+$names = ["Bob", "Jane", "Joe", "Peter"];
+
+$index = 0;
+while ($index < count($names)) {
+  print "Hello " . $names[$index] . "!<br />";
+  $index++;
+}
+
+// ALWAYS MAKE SURE TO HAVE A WAY FOR THE LOOP TO END!!!
+
+
