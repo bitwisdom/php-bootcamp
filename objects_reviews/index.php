96a696f7f2881fa382263d30536be30db6ec1dac
Wayne Eaker <wayne@zengenuity.com>
2016-04-21 11:15:26 -0400

    Simple OOP example.

---
+++ objects_reviews/index.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'Restaurant.php';
+
+$restaurant_one = new Restaurant();
+$restaurant_two = new Restaurant();
+
+var_dump($restaurant_one);
+var_dump($restaurant_two);
+var_dump($restaurant_one);
+
