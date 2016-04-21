8428b403d56b7e1a7d41a52a1d832ceefa243c55
Wayne Eaker <wayne@zengenuity.com>
2016-04-21 11:33:17 -0400

    Object with instance variable.

--- objects_reviews/Restaurant.php
+++ objects_reviews/Restaurant.php
@@ -1,5 +1,20 @@
 <?php
 
 class Restaurant {
-  
+    
+    private $name;
+    
+    public function __construct($name) {
+        $this->name = $name;
+    }
+    
+    
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
 }
--- objects_reviews/index.php
+++ objects_reviews/index.php
@@ -2,10 +2,12 @@
 
 include 'Restaurant.php';
 
-$restaurant_one = new Restaurant();
-$restaurant_two = new Restaurant();
+$restaurant_one = new Restaurant("China King");
+$restaurant_two = new Restaurant("Burger Shack");
 
-var_dump($restaurant_one);
-var_dump($restaurant_two);
-var_dump($restaurant_one);
+//var_dump($restaurant_one);
+//var_dump($restaurant_two);
+//var_dump($restaurant_one);
 
+print "Restaurant One: " . $restaurant_one->getName() . "<br />";
+print "Restaurant Two: " . $restaurant_two->getName() . "<br />";
