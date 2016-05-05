cc439b0faa150eebcc2ff291ca7fd37a478b7551
Wayne Eaker <wayne@zengenuity.com>
2016-05-05 13:08:33 -0400

    Beginning of Modern PHP refactor example

---
+++ shipments/import.php
@@ -0,0 +1,73 @@
+<?php
+
+if (count($argv) < 2) {
+    echo "Missing required argument: Deliveries CSV file\n";
+    die;
+}
+
+if (!file_exists($argv[1])) {
+    echo "Deliveries CSV file not found: " . $argv[1] . "\n";
+    die;
+}
+
+$fh = fopen($argv[1], 'r');
+$header = fgetcsv($fh);
+$deliveries = [];
+while($row = fgetcsv($fh)) {
+    $deliveries[$row[0]] = $row[1];
+}
+
+$db = new PDO("mysql:dbname=php_examples;host=localhost", "root", "root");
+
+foreach ($deliveries as $tracking_number => $delivery_date) {
+    echo "Updating delivery date for: " . $tracking_number . "\n";
+    $sth = $db->prepare("SELECT shipped FROM packages 
+            WHERE tracking_number = :tracking_number");
+    $sth->execute([
+        ':tracking_number' => $tracking_number,
+    ]);
+    $ship_date = $sth->fetchColumn();
+    
+    $number_of_shipping_days = number_of_shipping_days($ship_date, $delivery_date);
+    
+    $sth = $db->prepare(
+            "UPDATE packages SET delivered = :delivery_date, 
+                shipping_days = :shipping_days
+                WHERE tracking_number = :tracking_number"
+            );
+    
+    $sth->execute([
+        ':delivery_date' => $delivery_date,
+        ':shipping_days' => $number_of_shipping_days,
+        ':tracking_number' => $tracking_number,
+        ]);
+}
+
+echo "Updates finished.\n";
+
+
+function number_of_shipping_days($from, $to) {
+    $shippingDays = [1, 2, 3, 4, 5]; # date format = N (1 = Monday, ...)
+    $holidayDays = ['*-12-25', '*-01-01', '*-07-04']; # variable and fixed holidays
+    $holidayDays = array_flip($holidayDays);
+
+    $from = new DateTime($from);
+    $to = new DateTime($to);
+    $interval = new DateInterval('P1D');
+    $periods = new DatePeriod($from, $interval, $to);
+
+    $days = 0;
+    foreach ($periods as $period) {
+        if (!in_array($period->format('N'), $shippingDays)) {
+            continue;
+        }
+        if (isset($holidayDays[$period->format('Y-m-d')])) {
+            continue;
+        }
+        if (isset($holidayDays[$period->format('*-m-d')])) {
+            continue;
+        }
+        $days++;
+    }
+    return $days;
+}
